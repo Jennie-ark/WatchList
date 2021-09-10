4049ceecd3f91ac74a3e31b57eae7670f42e4cc3
Jennie Nii <anriasan@gmail.com>
2021-09-10 14:00:19 +0700

    update

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ import { MovieControls } from "./MovieControls";
 export const MovieCard = ({ movie, type }) => {
     return (
         <div className="movie-card">
-            <div className="overlay"></div>
+            <div className="overlay"/>
 
             <img
                 src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
